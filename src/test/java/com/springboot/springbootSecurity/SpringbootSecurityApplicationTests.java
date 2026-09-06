3d39c46dfae76677c50949decf9016563ecae410
Amanullah Saeed <55559732+Aman-Saeed@users.noreply.github.com>
2026-09-06 11:57:53 +0430

    test jwt token

--- src/test/java/com/springboot/springbootSecurity/SpringbootSecurityApplicationTests.java
+++ src/test/java/com/springboot/springbootSecurity/SpringbootSecurityApplicationTests.java
@@ -1,13 +1,30 @@
 package com.springboot.springbootSecurity;
 
+import com.springboot.springbootSecurity.entities.User;
+import com.springboot.springbootSecurity.services.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootSecurityApplicationTests {
 
+    @Autowired
+    private JwtService jwtService;
+
 	@Test
 	void contextLoads() {
+
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("saeed@gmail.com");
+        user.setPassword("123456");
+
+        String token = jwtService.generateToken(user);
+        System.out.println("Generated Token: " + token);
+
+        Long id = jwtService.getUserIdFromToken(token);
+        System.out.println("Extracted User ID from Token: " + id);
 	}
 
 }
