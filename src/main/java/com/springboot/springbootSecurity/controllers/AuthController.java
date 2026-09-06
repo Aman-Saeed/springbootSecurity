a4b525e78907e83e3bb003867f8939306282b0e6
Amanullah Saeed <55559732+Aman-Saeed@users.noreply.github.com>
2026-09-06 14:45:03 +0430

    setting token into the cookie

--- src/main/java/com/springboot/springbootSecurity/controllers/AuthController.java
+++ src/main/java/com/springboot/springbootSecurity/controllers/AuthController.java
@@ -5,6 +5,9 @@ import com.springboot.springbootSecurity.dto.SignUpDto;
 import com.springboot.springbootSecurity.dto.UserDto;
 import com.springboot.springbootSecurity.services.AuthService;
 import com.springboot.springbootSecurity.services.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,10 +31,17 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto
+                                        , HttpServletRequest request
+                                        , HttpServletResponse response) {
         // Implement your login logic here
 
         String token = authService.login(loginDto);
+
+        Cookie cookie = new Cookie("token", token);
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+
         return ResponseEntity.ok(token);
     }
 
