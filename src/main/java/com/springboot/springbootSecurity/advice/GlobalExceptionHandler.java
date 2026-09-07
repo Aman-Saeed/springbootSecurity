333c26e73ef4b2c1002c22209e5643a414fd454b
Amanullah Saeed <55559732+Aman-Saeed@users.noreply.github.com>
2026-09-07 15:06:58 +0430

    Jwt Exception

--- src/main/java/com/springboot/springbootSecurity/advice/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/springbootSecurity/advice/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.springboot.springbootSecurity.advice;
 
 
 import com.springboot.springbootSecurity.exceptions.ResourceNotFoundException;
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
@@ -23,6 +24,10 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
     }
 
-
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> handleJwtException(JwtException ex) {
+        ApiError apiError = new ApiError(ex.getLocalizedMessage(), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
+    }
 
 }
